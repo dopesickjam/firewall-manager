0e715930f5e32228910d112daeb2fecb3aef69e0
Oleg Kandyba <o.kandyba@admitad.com>
2021-12-22 16:53:26 +0200

    update structure

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import logging, argparse, sys, yaml, invoke
 from yaml.loader import SafeLoader
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+ips_yaml_path = 'templates/servers.yaml'
+groups_yaml_path = 'templates/groups.yaml'
+
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--config', dest='ufw_rules', help='path to ufw yaml rules', nargs=1, metavar=("FILE"))
 parser.add_argument('--flush', dest='flush', help='flush firewall before apply new rules', action="store_true")
+parser.add_argument('--flush_iptables', dest='flush_iptables', help='flush iptables!!! there is a risk of losing the connection', action="store_true")
 parser.add_argument('--remove', dest='remove', help='remove rule or ip', type=str)
-parser.add_argument('--servers_path', dest='servers_path', help='path to servers list', type=str)
 args = parser.parse_args()
 
 def read_yaml(config):
@@ -18,17 +21,20 @@ def read_yaml(config):
 def check_ufw(c):
     try:
         c.sudo('ufw --version')
-        logging.info(f'ufw installed for this server: {server_name}')
+        logging.info(f'ufw installed for this server: {target_server_ip}')
     except invoke.exceptions.UnexpectedExit:
-        logging.info(f'ufw not installed for this server: {server_name}')
+        logging.info(f'ufw not installed for this server: {target_server_ip}')
         c.sudo('apt-get install ufw -y')
 
 def ufw_reset(c):
     c.sudo('ufw --force disable')
     c.sudo('ufw --force reset')
+    logging.info(f'firewall was flushed for this server: {target_server_ip}')
+
+def iptables_reset(c):
     c.sudo('iptables -F')
     c.sudo('ip6tables -F')
-    logging.info(f'firewall was flushed for this server: {server_name}')
+    logging.info(f'iptables was flushed for this server: {target_server_ip}')
 
 def ufw_enable(c):
     c.sudo('ufw --force enable')
@@ -38,6 +44,7 @@ def ufw_add(c, ip, proto, port, comment, rule_type):
         logging.info(f'ufw {rule_type} from {ip} comment "{comment}"')
         c.sudo(f'ufw {rule_type} from {ip} comment "{comment}"')
     elif ip == 'any':
+        logging.info(f'ufw {rule_type} proto {proto} to any port {port} comment "{comment}"')
         c.sudo(f'ufw {rule_type} proto {proto} to any port {port} comment "{comment}"')
     else:
         for item in port.split(','):
@@ -55,74 +62,56 @@ def ufw_remove(c, remove_rule):
                 logging.info(f'find rule with number: {number}')
                 c.sudo(f'ufw --force delete {number}')
             else:
-                logging.info(f'{server_name} clean from rule: {remove}')
+                logging.info(f'{target_server_ip} clean from rule: {remove}')
                 break
 
-def get_variables(server_rules, rules_type, rule):
-    port = server_rules['ufw_simple'][rule][rules_type]['to_port'] if 'to_port' in server_rules['ufw_simple'][rule][rules_type] else 'any'
-    proto = server_rules['ufw_simple'][rule][rules_type]['proto'] if 'proto' in server_rules['ufw_simple'][rule][rules_type] else 'any'
-    from_list = server_rules['ufw_simple'][rule][rules_type]['from'] if 'from' in server_rules['ufw_simple'][rule][rules_type] else 'any'
+def get_ip(global_name, group, name):
+    return ips_yaml[global_name][group][name]
 
-    return  port, proto, from_list
+ips_yaml = read_yaml(ips_yaml_path)
+groups_yaml = read_yaml(groups_yaml_path)
 
+logging.info(f'Render config from {args.ufw_rules[0]}')
+server_rules = read_yaml(args.ufw_rules[0])
 
-if args.ufw_rules:
-    logging.info(f'Render config from {args.ufw_rules[0]}')
+if server_rules['ufw_simple']['enabled']:
+    server_path = server_rules['ufw_simple']['host'].split('.')
 
-    server_rules = read_yaml(args.ufw_rules[0])
+    target_server_ip = get_ip(server_path[0],server_path[1],server_path[2])
+    target_server_user = server_rules['ufw_simple']['user_ssh']
+    target_server_port = server_rules['ufw_simple']['port_ssh']
 
-    all_servers = read_yaml(args.servers_path)
+    c = Connection(host=target_server_ip, user=target_server_user, port=target_server_port)
 
-    server_name = server_rules['server']['name']
-    server_user = server_rules['server']['user']
-    server_port = server_rules['server']['port']
-    c = Connection(host=server_name, user=server_user, port=server_port)
+    check_ufw(c)
 
-    ufw_status = server_rules['ufw_simple']['enabled']
+    if args.flush:
+        ufw_reset(c)
 
-    if ufw_status:
-        check_ufw(c)
+    if args.flush_iptables:
+        iptables_reset(c)
 
-        if args.flush:
-            ufw_reset(c)
+    if args.remove:
+        ufw_remove(c, args.remove)
 
-        if args.remove:
-            ufw_remove(c, args.remove)
+    allow_groups = server_rules['ufw_simple']['allow']
 
-        for allow_rules in server_rules['ufw_simple']['allow']:
-            port, proto, from_list = get_variables(server_rules, allow_rules, 'allow')
+    for allow_group in allow_groups:
+        for group in groups_yaml['groups']:
+            if group['name'] == allow_group:
+                protocol = group['proto']
+                ports = str(group['ports']).split(',')
+                allowed_servers = group['ips']
 
-            if from_list == 'any':
-                ufw_add(c, 'any', proto, port, 'open for world', 'allow')
+                for port in ports:
+                    if allowed_servers == 'any':
+                        ufw_add(c, 'any', protocol, port, 'world', 'allow')
+                    else:
+                        for ip in allowed_servers:
+                            ip = ip.split('.')
+                            server_ip = get_ip(ip[0], ip[1], ip[2])
+                            ufw_add(c, server_ip, protocol, port, ip[2], 'allow')
 
-            for server in from_list:
-                server = server.split('.')
-
-                if len(server) == 2:
-                    multi_rule = all_servers[server[0]][server[1]]
-                    for rule in multi_rule:
-                        ufw_add(c, multi_rule[rule], proto, port, rule, 'allow')
-                if len(server) == 3:
-                    single_rule = all_servers[server[0]][server[1]][server[2]]
-                    ufw_add(c, single_rule, proto, port, server[2], 'allow')
-
-        rules = server_rules['ufw_simple']['deny'] if 'deny' in server_rules['ufw_simple'] else 'any'
-        if not 'any' in rules:
-            for deny_rules in server_rules['ufw_simple']['deny']:
-                port, proto, from_list = get_variables(server_rules, deny_rules, 'deny')
-
-                for server in from_list:
-                    server = server.split('.')
-
-                    if len(server) == 2:
-                        multi_rule = all_servers[server[0]][server[1]]
-                        for rule in multi_rule:
-                            ufw_add(c, multi_rule[rule], proto, port, rule, 'deny')
-                    if len(server) == 3:
-                        single_rule = all_servers[server[0]][server[1]][server[2]]
-                        ufw_add(c, single_rule, proto, port, server[2], 'deny')
-
-        ufw_enable(c)
-                    
-    else:
-        logging.info(f'ufw disable for this server: {server_name}')
+    ufw_enable(c)
+else:
+    logging.info(f'ufw disable for {args.ufw_rules[0]}')
